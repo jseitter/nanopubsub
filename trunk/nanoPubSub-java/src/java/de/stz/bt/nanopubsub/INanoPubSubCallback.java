dbe8398b3315ccf07baa4613d233cba6aac03f0f
jseitter <jseitter@65dfcb70-8b2d-4fda-a9ae-60b9b9e0a85a>
2008-09-11 12:11:13 +0000

    Initial import.

---
+++ trunk/nanoPubSub-java/src/java/de/stz/bt/nanopubsub/INanoPubSubCallback.java
@@ -0,0 +1,25 @@
+/*
+ *   nanoPubSub - embedded Publish Subscribe Messaging
+ *   (c) 2008 STZ Building Technology
+ * 
+ *   nanoPubSub is free software; you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation; version 2 of the License.
+ * 
+ *   This program is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details.
+ */
+
+package de.stz.bt.nanopubsub;
+
+/**
+ * Callback handlers must implement this interface
+ * @author jseitter
+ */
+public interface INanoPubSubCallback {
+
+	public void onMessage(String topic, String message);
+	
+}
